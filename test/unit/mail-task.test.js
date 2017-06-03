2b561559a72c4f67f2467c1d6e4e9939670abc53
lucent1090 <lucent1090@gmail.com>
2017-06-04 02:50:39 +0800

    update unit test

--- test/unit/mail-task.test.js
+++ test/unit/mail-task.test.js
@@ -6,12 +6,42 @@ import { describe, it, before, after } from 'mocha'
 
 import fs from 'fs'
 import mailtask from '../mail-task'
+import smtp from '../smtp-server'
+import kueServer from '../kue-server'
 
 describe('mail-task test', () => {
-  describe('start function', () => {
-
-  })
   describe('setWorker function', () => {
+    let mailServer,
+        taskServer,
+        config,
+        fakeTaskFile,
+        setJobTypeListSpy,
+        getTransporterStub
 
+    before(() => {
+      mailServer = smtp()
+      taskServer = kueServer()
+      setJobTypeListSpy = sinon.spy(taskServer, 'setJobTypeList')
+      getTransporterStub = sinon.stub(mailServer, 'getTransporter')
+      getTransporterStub.returns({
+        connectionTimeout: 3000
+      })
+      
+      config = JSON.parse( fs.readFileSync('./src/config.json') )
+
+      fakeTaskFile = {
+        type: 'TEST',
+        kue_config: {
+          MAX_PROCESS_JOB: null
+        },
+        transporter: null,
+        worker: () => {},
+        setConfig: (config) => {
+          this.kue_config = config.kue_config.MAX_PROCESS_JOB
+        }
+      }
+    })
+
+    it('should')
   })
 })
